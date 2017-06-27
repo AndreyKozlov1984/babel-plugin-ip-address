0f3974a2292b980911dc9fd6527881f2a3a43609
ZeusTheTrueGod <l-2732@yandex.ru>
2017-06-27 17:55:15 +0300

    one more fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const _ = require('lodash');
 const ip = require('ip');
 const ipAddress = ip.address();
-const ifaces = require('os').networkIntrfaces();
+const ifaces = require('os').networkInterfaces();
 const macAddress = _.find(_.flatten(_.values(ifaces)), { address : '192.168.1.39'}).mac;
 module.exports =  ({ types }) => {
     return {
