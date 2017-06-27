fb2e39ec1106ceed152cb539c1740206e5f326ca
ZeusTheTrueGod <l-2732@yandex.ru>
2017-06-27 17:21:55 +0300

    providing a mac address too

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
+import _ from 'lodash';
 const ip = require('ip');
+const ipAddress = ip.address();
+const macAddress = _.find(_.flatten(_.values(ifaces)), { address : '192.168.1.39'}).mac
 module.exports =  ({ types }) => {
     return {
         visitor: {
             ReferencedIdentifier (path) {
                 if (path.node.name === '__IP_ADDRESS__') {
-                    path.replaceWith(types.stringLiteral(ip.address()));
+                    path.replaceWith(types.stringLiteral(ipAddress));
+                }
+                if (path.node.name === '__MAC_ADDRESS__') {
+                    path.replaceWith(types.stringLiteral(macAddress));
                 }
             }
         }
